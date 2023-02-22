edbe7da61ab043dea3065540a07a5cb52099e9d6
alek <alekart@users.noreply.github.com>
2023-02-22 22:55:26 +0100

    Provide nunjucks globals, filters and extension via loader options

--- src/index.js
+++ src/index.js
@@ -33,6 +33,45 @@ export default function loader(source) {
    */
   const nunjucksFiles = new (getNunjucksFileSystemLoader(this))(templatesPath);
   const env = new nunjucks.Environment(nunjucksFiles, nunjucksOptions);
+  addToEnvFromOptions(options, env);
 
   return env.renderString(source, data);
+}
+
+/**
+ * @param options {Record<string, any>}
+ * @param env {Environment}
+ */
+function addToEnvFromOptions(options, env) {
+  const {globals, extensions, filters} = options;
+  Object.keys(globals || {}).forEach((name) => {
+    addToEnv('global', name, globals[name], env);
+  });
+  Object.keys(extensions || {}).forEach((name) => {
+    addToEnv('extension', name, globals[name], env);
+  });
+  Object.keys(filters || {}).forEach((name) => {
+    addToEnv('filter', name, globals[name], env);
+  });
+}
+
+/**
+ * @param type {"global"|"extension"|"filter"}
+ * @param name {string}
+ * @param fn {Function}
+ * @param env {Environment}
+ */
+function addToEnv(type, name, fn, env) {
+  // eslint-disable-next-line default-case
+  switch (type) {
+    case 'global':
+      env.addGlobal(name, fn);
+      break;
+    case 'extension':
+      env.addExtension(name, fn);
+      break;
+    case 'filter':
+      env.addFilter(name, fn);
+      break;
+  }
 }
