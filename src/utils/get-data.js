c6d2d1210f01eb95bf5faf5b034a6c984a331fe3
alek <alekart@users.noreply.github.com>
2023-03-28 15:28:24 +0200

    Remove lodash usage in favor of home-made scripts

--- src/utils/get-data.js
+++ src/utils/get-data.js
@@ -1,16 +1,14 @@
 import { basename } from 'path';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { merge } from 'lodash';
-
 import loadData from './load-data';
 import loadPartials from './load-partials-data';
+import mergeDeep from './merge-deep';
 
 export default function getData(loader, dataPath) {
   const options = loader.getOptions();
   const {locale} = options;
   const templateName = basename(loader.resourcePath).replace(/\.\w+$/, '');
-  return merge(
+  return mergeDeep(
     // global data load
     loadData('global', dataPath, loader),
     locale ? loadData('global', dataPath, loader, locale) : {},
--- src/utils/load-partials-data.js
+++ src/utils/load-partials-data.js
@@ -1,17 +1,14 @@
 import { basename, join } from 'path';
-
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { sync as globSync } from 'glob';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { camelCase } from 'lodash';
 
+import camelCase from './camel-case';
 import loadJsonData from './load-json-file';
 
 export default function loadPartials(dataPath, loader, locale = '') {
   const localePath = locale ? `/${locale}/` : '';
   const globPath = join(dataPath, localePath, `_*.json`);
   return globSync(globPath).reduce((partials, filename) => {
-    const varName = `_${  camelCase(basename(filename).replace(/^_/, '').replace('.json', ''))}`;
+    const varName = `_${camelCase(basename(filename).replace(/^_/, '').replace('.json', ''))}`;
     loader.addDependency(filename);
     // eslint-disable-next-line no-param-reassign
     partials[varName] = loadJsonData(filename);
